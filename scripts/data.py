5f2ca83343b4e484e3482fe103c08705d8f1e977
Sejin Nam <sejin8642@gmail.com>
2021-09-06 17:12:40 -1000

    added ftnmr.py for data.py

--- scripts/data.py
+++ scripts/data.py
@@ -1,12 +1,14 @@
-# python  script to generate random data set
+# python  script to generate random data set using ftnmr module
 from ftnmr import *
 import h5py
 import concurrent.futures as cf
 
 def generate(n, m):
-    with h5py.File(f"{ 
+    with h5py.File(f"{n}", 'w') as f:
+        pass
 
 def main():
     with cf.ProcessPoolExecutor() as executor:
-        
+        pass
+
 if __name__ == '__main__': main()
