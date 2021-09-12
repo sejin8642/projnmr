5ecf83a0c35e07f16b133f79fd5b7a84022e0d0f
Sejin Nam <sejin8642@gmail.com>
2021-09-11 18:21:58 -1000

    trying to figure out how to generate random molecules

--- scripts/data.py
+++ scripts/data.py
@@ -1,14 +1,36 @@
-# python  script to generate random data set using ftnmr module
-from ftnmr import *
+# python  script to generate random data set using ftnmr module with baseline artifact
 import h5py
-import concurrent.futures as cf
+from ftnmr import *
+from concurrent import futures
 
-def generate(n, m):
-    with h5py.File(f"{n}", 'w') as f:
+def generate(n, m, b, name='data'):
+    """
+    generate function simulates ftnmr.measure output and saves it as hdf5 files.
+    
+    Parameters
+    ----------
+    n: int
+        Number of processed sample signal in each data block
+    m: int
+        m-th block of dataset
+    b: int
+        Number of data blocks
+    name: str
+        Saved data file name (default data)
+    """
+    spec = spectrometer(t_cut=1500, std=0.00001)
+    data = np.zeros((n, spec.nf))
+    for i in range(n):
+        molecules = _
+        spec.artifact(baseline=True)
+        spec.measure(moles=molecules)
+        data[i] = spec.spectra
+
+    with h5py.File(name + str(m).zfill(len(str(b))), 'w') as f:
         pass
 
 def main():
-    with cf.ProcessPoolExecutor() as executor:
+    with futures.ProcessPoolExecutor() as executor:
         pass
 
 if __name__ == '__main__': main()
---
+++ scripts/projnmr.py
@@ -0,0 +1,17 @@
+# A list of functions for ftnmr.spectrometer
+import sys
+sys.path.insert(1, '/home/sejin8642/gd/ftnmr/scripts')
+
+import numpy as np
+from ftnmr import spectrometer
+
+def molecules():
+    """
+    Generates random molecules 
+
+    return
+    ------
+    molecules: dict[str]:(molecule, float)
+        Dictionary of molecules with their relative abundances
+    """
+    pass
