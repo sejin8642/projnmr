d45b641000a34c8587db3bc1178bce3d06759b96
Sejin Nam <sejin8642@gmail.com>
2021-10-29 14:47:48 -1000

    first commit after a long hiatus

--- scripts/projnmr.py
+++ scripts/projnmr.py
@@ -9,9 +9,13 @@ def molecules():
     """
     Generates random molecules 
 
+    Things to consider before modification
+    hydrogens = {str:(total number, chemical shift)}
+    couplings = (str, str, J-coupling) 
     return
     ------
     molecules: dict[str]:(molecule, float)
         Dictionary of molecules with their relative abundances
     """
     pass
+
