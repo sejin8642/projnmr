af1c53d3ebe07085457af22c520ca548670c50f5
Sejin Nam <sejin8642@gmail.com>
2021-12-15 20:57:42 -1000

    worked on molecule generator almost done

--- scripts/projnmr.py
+++ scripts/projnmr.py
@@ -5,7 +5,7 @@ sys.path.insert(1, '/home/sejin8642/gd/ftnmr/scripts')
 import numpy as np
 from ftnmr import spectrometer
 
-def moleculesGenerator(N = 9):
+def moleculesGenerator(min_N = 4, max_N = 10):
     """
     Generates random molecules 
 
@@ -15,8 +15,10 @@ def moleculesGenerator(N = 9):
 
     Parameters
     ----------
-    N: int
+    min_N: int
         Minimum number of hydrogen groups in a molecule
+    max_N: int
+        Maximum number of hydrogen groups in a molecule
 
     return
     ------
@@ -24,26 +26,96 @@ def moleculesGenerator(N = 9):
         Dictionary of molecules with their relative abundances
     """
     # hydrogen clusters (first and later) for groups list and their size
-    FG = [[3], [3, 3], [3, 2, 3], [3, 2, 2, 3], [3, 3, 1, 3]]
-    MG = [[2], [2, 3], [2, 2, 3], [2, 2, 2, 3], [2, 3, 1, 3]]
-    size = [1, 2, 3, 4, 4] 
+    backbones = [
+            'methane',
+            'ethane', 
+            'ethylene', 
+            'propane', 
+            'propene1',
+            'propene2',
+            'butane',
+            'butene1',
+            'butene2',
+            'butene3',
+            'isobutane']
+    FG = [
+            [3],
+            [3, 3], 
+            [2, 2],
+            [3, 2, 3],
+            [2, 1, 3],
+            [3, 1, 2],
+            [3, 2, 2, 3], 
+            [2, 1, 2, 3],
+            [3, 1, 1, 3],
+            [3, 2, 1, 2],
+            [3, 3, 1, 3]]
+    MG = [
+            [2],
+            [2, 3], 
+            [1, 2],
+            [2, 2, 3],
+            [1, 2, 3],
+            [2, 2, 2],
+            [2, 2, 2, 3], 
+            [1, 1, 2, 3],
+            [2, 1, 1, 3],
+            [2, 2, 1, 2],
+            [2, 3, 1, 3]]
+    size = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4] 
    
     # index for the first group
-    ind = np.random.randint(0, 5)
-    groups = FG[ind] # hydrogen groups with their hydrogen count
+    ind = np.random.randint(0, 11)
+    indices = [ind] # indices of backbones in order
     length = size[ind] # length of groups 
+    groups = FG[ind] # hydrogen groups with their hydrogen count
     cut = [length] # indices for cluster separation
+    isobutane = [ind//10] # to indicate which cluster is isobutane backbone
 
-    while (length < N):
+    # condition for while loop for more clusters
+    def cond():
+        if length < min_N:
+            return True
+        else:
+            return bool(np.random.randint(0, 2))
+
+    # backbone attachment for multiple clusters
+    while (cond()):
         # reduce one hydrogen from one group if attached to molecular backbone group
         ind = np.random.randint(0, length)
         if groups[ind] != 1: groups[ind] -= 1 
 
         # index for later groups
-        ind = np.random.randint(0, 5)
-        groups.extend(MG[ind])
+        ind = np.random.randint(0, 11)
+        if max_N < length + size[ind]:
+            break
+        indices.append(ind)
         length += size[ind]
+        groups.extend(MG[ind])
         cut.append(length)
+        isobutane.append(ind//10)
 
-    return groups, cut
+    # get rid of duplicate methyl groups for isobutane backbone
+    for n, x in enumerate(isobutane):
+        if x == 1:
+            if (groups[cut[n]-1] == 3) and (groups[cut[n]-3] == 3):
+                groups[cut[n]-1] = np.random.randint(1, 3) 
 
+    # reduce one hydrogen from one random group multiple random times 
+    while (bool(np.random.randint(0, 2))):
+        ind = np.random.randint(0, length)
+        if groups[ind] != 1: groups[ind] -= 1 
+
+    # create equivalent hydrogen groups randomly and combine their total hydrogen count
+    FG = [
+            [3, 3], 
+            [3, 2, 3],
+            [3, 2, 2, 3], 
+            [2, 1, 2, 3],
+            [3, 2, 1, 2]]
+    equi = [1, 3, 6, 7, 9]
+    for n, ind in enumerate(indices):
+        if ind in equi:
+            if groups(cut[n]
+
+    return groups, cut, isobutane, indices
